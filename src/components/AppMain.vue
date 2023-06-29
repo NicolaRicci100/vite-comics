bdd2e23b5ce5231cd0ff5dc47bbac4c4fc06ac95
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-29 19:33:15 +0200

    aggiunta scritte in main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -40,38 +40,38 @@ export default {
     <div class="container main-footer">
       <div>
         <ul><strong>DC COMICS</strong>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>News</li>
         </ul>
         <ul><strong>SHOP</strong>
-          <li>lorem</li>
-          <li>lorem</li>
+          <li>Shop DC</li>
+          <li>Shop DC Collectibles</li>
         </ul>
       </div>
       <ul><strong>DC</strong>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
+        <li>Terms of Use</li>
+        <li>Privacy policy (New)</li>
+        <li>Ad Choices</li>
+        <li>Advertising</li>
+        <li>Jobs</li>
+        <li>Subscriptions</li>
+        <li>Talent Workshops</li>
+        <li>CPSC Certificates</li>
+        <li>Ratings</li>
+        <li>Shop help</li>
+        <li>Contact Us</li>
       </ul>
       <ul><strong>SITES</strong>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
-        <li>lorem</li>
+        <li>DC</li>
+        <li>MAD Magazine</li>
+        <li>DC Kids</li>
+        <li>DC Universe</li>
+        <li>DC Power Vista</li>
       </ul>
     </div>
   </section>
@@ -139,6 +139,8 @@ section {
 .main-footer ul li {
   font-size: 12px;
   font-weight: 300;
+  padding: 2px 0;
+  color: gainsboro;
 }
 
 ul li {
